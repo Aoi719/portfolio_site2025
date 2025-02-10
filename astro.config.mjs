06e7ad706f17924af2d1875ec869838694a96c55
Aoi719 <n1sh1_works@yahoo.co.jp>
2025-02-11 00:08:02 +0900

    環境設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,36 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          additionalData: `
+            @use "src/styles/mixins/_breakpoints.scss" as *;
+            @use "src/styles/settings/_variables.scss" as *;
+          `
+        }
+      }
+    },
+    build: {
+      cssMinify: false,
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          assetFileNames: assetInfo => {
+            const fileName = assetInfo?.names;
+            if (fileName && Array.isArray(fileName)) {
+              const extType = fileName[0].split('.').at(-1);
+              if (extType === 'css') {
+                return 'css/style.css';
+              }
+            }
+            return `assets/[name][extname]`;
+          },
+        },
+      }
+    }
+  }
+});
